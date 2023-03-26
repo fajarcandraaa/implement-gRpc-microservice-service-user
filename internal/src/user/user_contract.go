41d60af4becbd123aaec90755367a4f5bad26dec
fajarcandra <fajar.candra@privyid.tech>
2023-03-27 01:38:29 +0700

    clean code

--- internal/src/user/user_contract.go
+++ internal/src/user/user_contract.go
@@ -10,7 +10,4 @@ import (
 type Service interface {
 	InsertNewUser(ctx context.Context, payload *proto.CreateUserRequest) (*userentity.User, error)
 	FindUser(ctx context.Context, userID string) (*userentity.User, error)
-	// GetListUsers(ctx context.Context, sortBy, orderBy string, perPage, page int) (*[]userentity.Users, int64, error)
-	// UpdateUser(ctx context.Context, payload *userentity.UserData) (*userentity.UserData, error)
-	// DeleteDataUser(ctx context.Context, userID string) error
 }
